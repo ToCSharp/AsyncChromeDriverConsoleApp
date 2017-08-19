ad8cfe6029a917fbb0a95079b761d98ae5cff144
Oleg Zudov <tocsharp@gmail.com>
2017-08-19 12:51:48 +0300

    In static void Main CaptureScreenshot in other thread works

--- AsyncChromeDriverConsoleApp/Program.cs
+++ AsyncChromeDriverConsoleApp/Program.cs
@@ -16,47 +16,7 @@ namespace AsyncChromeDriverConsoleApp
     class Program
     {
 
-        //static void Main(string[] args)
-        //{
-        //    //Launch Chrome With
-
-        //    //"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9223
-
-        //    Console.WriteLine("Hello World!");
-
-        //    var sessions = GetSessions("http://localhost:9223/").GetAwaiter().GetResult();
-
-        //    using (var session = new ChromeSession(sessions.First(s => s.Type == "page").WebSocketDebuggerUrl))
-        //    {
-        //        try
-        //        {
-        //            //session.Page.SubscribeToLoadEventFiredEvent((e) =>
-        //            session.Page.SubscribeToDomContentEventFiredEvent((e) =>
-        //            {
-        //                Console.WriteLine("Page loaded");
-        // NOT WORKS
-        //                var screenshot = session.Page.CaptureScreenshot(new Page.CaptureScreenshotCommand(), new System.Threading.CancellationToken(), 5000).GetAwaiter().GetResult();
-        //                SaveScreenshot(screenshot.Data);
-
-        //            });
-        //            session.Page.Enable(new Page.EnableCommand()).GetAwaiter().GetResult();
-        //            var navigateResult = session.Page.Navigate(new Page.NavigateCommand
-        //            {
-        //                Url = "https://www.google.com/"
-        //            }).GetAwaiter().GetResult();
-        //            Console.ReadLine();
-        //            session.Page.Disable(new Page.DisableCommand()).GetAwaiter().GetResult();
-
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            Console.WriteLine(ex.ToString());
-        //        }
-        //    }
-        //}
-
-        //C# 7.1
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
             //Launch Chrome With
 
@@ -64,25 +24,36 @@ namespace AsyncChromeDriverConsoleApp
 
             Console.WriteLine("Hello World!");
 
-            var sessions = await GetSessions("http://localhost:9223/"); //.GetAwaiter().GetResult();
+            var sessions = GetSessions("http://localhost:9223/").GetAwaiter().GetResult();
 
             using (var session = new ChromeSession(sessions.First(s => s.Type == "page").WebSocketDebuggerUrl))
             {
                 try
                 {
-                    await session.Page.Enable(new Page.EnableCommand());
-                    //session.Page.SubscribeToLoadEventFiredEvent(async (e2) =>
-                    session.Page.SubscribeToDomContentEventFiredEvent(async (e2) =>
+                    //session.Page.SubscribeToLoadEventFiredEvent((e) =>
+                    session.Page.SubscribeToDomContentEventFiredEvent((e) =>
                     {
-                        var screenshot = await session.Page.CaptureScreenshot(new Page.CaptureScreenshotCommand());
-                        SaveScreenshot(screenshot.Data);
+                        Console.WriteLine("Page loaded");
+                        ////NOT WORKS
+                        //var screenshot = session.Page.CaptureScreenshot(new Page.CaptureScreenshotCommand(), new System.Threading.CancellationToken(), 5000).GetAwaiter().GetResult();
+                        //SaveScreenshot(screenshot.Data);
+
+                        // WORKS
+                        Task.Run(() =>
+                        {
+                            var screenshot = session.Page.CaptureScreenshot(new Page.CaptureScreenshotCommand(), new System.Threading.CancellationToken(), 5000).GetAwaiter().GetResult();
+                            SaveScreenshot(screenshot.Data);
+
+                        });
+
                     });
-                    var navigateResult = await session.Page.Navigate(new Page.NavigateCommand
+                    session.Page.Enable(new Page.EnableCommand()).GetAwaiter().GetResult();
+                    var navigateResult = session.Page.Navigate(new Page.NavigateCommand
                     {
                         Url = "https://www.google.com/"
-                    });
+                    }).GetAwaiter().GetResult();
                     Console.ReadLine();
-                    //await session.Page.Disable(new Page.DisableCommand());
+                    session.Page.Disable(new Page.DisableCommand()).GetAwaiter().GetResult();
 
                 }
                 catch (Exception ex)
@@ -91,6 +62,44 @@ namespace AsyncChromeDriverConsoleApp
                 }
             }
         }
+
+        ////C# 7.1
+        //static async Task Main(string[] args)
+        //{
+        //    //Launch Chrome With
+
+        //    //"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9223
+        //    //"C:\Program Files\Opera\47.0.2631.55\opera.exe" --remote-debugging-port=9223
+
+        //    Console.WriteLine("Hello World!");
+
+        //    var sessions = await GetSessions("http://localhost:9223/"); //.GetAwaiter().GetResult();
+
+        //    using (var session = new ChromeSession(sessions.First(s => s.Type == "page").WebSocketDebuggerUrl))
+        //    {
+        //        try
+        //        {
+        //            //await session.Page.Enable(new Page.EnableCommand());
+        //            ////session.Page.SubscribeToLoadEventFiredEvent(async (e2) =>
+        //            //session.Page.SubscribeToDomContentEventFiredEvent(async (e2) =>
+        //            //{
+        //            //    var screenshot = await session.Page.CaptureScreenshot(new Page.CaptureScreenshotCommand());
+        //            //    SaveScreenshot(screenshot.Data);
+        //            //});
+        //            var navigateResult = await session.Page.Navigate(new Page.NavigateCommand
+        //            {
+        //                Url = "https://www.bing.com/"
+        //            });
+        //            Console.ReadLine();
+        //            //await session.Page.Disable(new Page.DisableCommand());
+
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            Console.WriteLine(ex.ToString());
+        //        }
+        //    }
+        //}
 
         public static async Task<ICollection<ChromeSessionInfo>> GetSessions(string chromeUrl)
         {
